94883212708b8c7c932b6dbeb501882d355c2b89
Jyri Seepter <jyri.seepter@gmail.com>
2026-03-06 18:38:00 +0200

    simplify stripe init

--- app/api/stripe/portal/route.ts
+++ app/api/stripe/portal/route.ts
@@ -8,7 +8,7 @@ if (!stripeSecretKey) {
   throw new Error("Missing STRIPE_SECRET_KEY in environment variables");
 }
 
-const stripe = new Stripe(stripeSecretKey, {
+const stripe = new Stripe(stripeSecretKey);
 
 });
 
